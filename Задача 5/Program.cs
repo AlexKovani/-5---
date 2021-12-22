40c769301015ae5133fdacbb0a778baa5423d502
canya <canya@DESKTOP-0RBMO97>
2021-12-22 17:17:59 +0300

    Добавьте файлы проекта.

---
+++ Задача 5/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Обязательная задача*.
+              //Запросить у пользователя целочисленное значение N.
+              //Сформировать двумерный массив размера NxN следующего вида:
+
+              1 0 1 0 1
+
+              0 1 0 1 0
+
+              1 0 1 0 1
+
+              0 1 0 1 0
+
+              1 0 1 0 1 */
+
+            //"Решение"
+            // 1. Ввести значение N с клавиатуры
+            // 2. Создать массив размера NxN
+            // 3. Задать массиву конкретные значения 1 и 0
+            // * нечетная строка: 1 0 1 0 1  * четная строка 0 1 0 1 0 
+            // 4. Пронумеровать их в требуемой последовательности
+            Console.Write("Введите значение n, что бы задать кол-во строк и столбцов таблице: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            int[,] mas = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (j % 2 != 0)
+                        {
+                            Console.Write("{0,2}", 1);
+                        }
+                        else
+                        {
+                            Console.Write("{0,2}", 0);
+                        }
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (j % 2 != 0)
+                        {
+                            Console.Write("{0,2}", 0);
+                        }
+                        else
+                        {
+                            Console.Write("{0,2}", 1);
+                        }
+                    }
+                }
+                Console.WriteLine();
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("Cпасибо!");
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
---
+++ Урок 5 - Массивы/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Сформировать одномерный массив из 7 элементов.
+            //  Заполнить массив числами, вводимыми с клавиатуры, определить среднее арифметическое элементов.
+
+            //"Решение"
+            // 1. создаю и инициализирую массив num
+            // 2. ввожу 7 элементов массива
+            // 3. считаю сумму всех введенных элементов
+            // 4. вывожу на экран число ср.арифмет. значения = Сум/7
+
+            //1.
+             const int n = 7;
+             int[] num = new int[n];
+             //2.
+             Console.WriteLine("Введите 7 чисел, через Enter :");
+             for (int i = 0; i < n; i++)
+             {
+              num[i] = Convert.ToInt32(Console.ReadLine());
+             }
+             Console.WriteLine();
+             //3.
+             int S = 0;
+             for (int i = 0; i < n; i++)
+             {
+                 S += num[i];
+             }
+             //4.
+             Console.WriteLine("Ср. Арифметич. введенных чисел = " + S/n);
+             Console.ReadKey();
+
+            }
+    }
+}
+
